9c601f2e7978a6c19958ceb4bcd69728ff4d595a
Abi1024 <abiyaz.chowdhury@stonybrook.edu>
2019-07-06 10:59:28 -0400

    Fixed a bug with max hit calculator where equipedItems would sometimes have less than 14 entries.

--- runelite-client/src/main/java/net/runelite/client/plugins/maxhit/equipment/EquipmentHelper.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/maxhit/equipment/EquipmentHelper.java
@@ -35,26 +35,24 @@ public class EquipmentHelper
 		return itemSet.getItems().stream().allMatch(item -> wearsItem(equipedItems, item));
 	}
 
-	private static boolean wearsItem(Item[] equipedItems, int itemId)
-	{
-		//System.out.println("2nd equipment slot item");
-		for (int i = 0; i < equipedItems.length; i++){
-			if (equipedItems[i].equals(itemId)){
-				return true;
-			}
-		}
-		return false;
-	}
+    private static boolean wearsItem(Item[] equipedItems, EquipmentInventorySlot slot, int itemId)
+    {
+        if (slot.getSlotIdx() >= equipedItems.length){
+            return false;
+        }
+        Item item = equipedItems[slot.getSlotIdx()];
+        if (item == null)
+        {
+            return false;
+        }
+        return item.getId() == itemId;
+    }
 
-	public static boolean wearsItem(Item[] equipedItems, EquipmentSlotItem equipmentSlotItem)
-	{
-		//System.out.println("3rd wears item: ");
-        /*for (Integer i : equipmentSlotItem.getItems()){
-            System.out.println("Item ID: " + i);
-        }*/
-		return equipmentSlotItem.getItems().stream().anyMatch(itemId ->
-				wearsItem(equipedItems,itemId)
-		);
-	}
+    public static boolean wearsItem(Item[] equipedItems, EquipmentSlotItem equipmentSlotItem)
+    {
+        return equipmentSlotItem.getItems().stream().anyMatch(itemId ->
+                wearsItem(equipedItems, equipmentSlotItem.getEquipmentSlot(), itemId)
+        );
+    }
 
 }
