cc913f9e719f1469db01eac500847a9753c4d68b
Abi1024 <abiyaz.chowdhury@stonybrook.edu>
2019-08-05 01:00:06 -0400

    Added minimap overlays. Supply spots full functionality added and bugs fixed.

--- runelite-client/src/main/java/net/runelite/client/plugins/gauntlethelper/MinimapOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/gauntlethelper/MinimapOverlay.java
@@ -1,6 +1,6 @@
 package net.runelite.client.plugins.gauntlethelper;
 
-import net.runelite.api.*;
+import net.runelite.api.GameObject;
 import net.runelite.api.Point;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
@@ -35,89 +35,44 @@ public class MinimapOverlay extends Overlay {
         if (config.supplySpots()){
             renderSupplySpots(graphics);
         }
-        if (config.bossMonsters()){
-            renderMinibosses(graphics);
-        }
-        if (config.highlightMonsters()){
-            renderMonsters(graphics);
-        }
         return null;
     }
 
     private void renderSupplySpots(Graphics2D graphics){
-        for (GameObject spot : plugin.getResource_spots().values()){
-            switch (spot.getId()){
-                case ObjectID.FISHING_SPOT_36068:
-                case ObjectID.FISHING_SPOT_35971:
-                    if (plugin.getSupplies().fish < config.num_fish()){
-                        renderMinimapObject(graphics, config.supplySpotsColor(), spot);
-                    }
-                    break;
-                case ObjectID.CRYSTAL_DEPOSIT:
-                case ObjectID.CORRUPT_DEPOSIT:
-                    if (plugin.getSupplies().ore < config.num_resources()){
-                        renderMinimapObject(graphics, config.supplySpotsColor(), spot);
-                    }
-                    break;
-                case ObjectID.LINUM_TIRINUM:
-                case ObjectID.LINUM_TIRINUM_36072:
-                    if (plugin.getSupplies().linum < config.num_resources()){
-                        renderMinimapObject(graphics, config.supplySpotsColor(), spot);
-                    }
-                    break;
-                case ObjectID.PHREN_ROOTS:
-                case ObjectID.PHREN_ROOTS_36066:
-                    if (plugin.getSupplies().bark < config.num_resources()){
-                        renderMinimapObject(graphics, config.supplySpotsColor(), spot);
-                    }
-                    break;
-                case ObjectID.GRYM_ROOT:
-                case ObjectID.GRYM_ROOT_36070:
-                    if (plugin.getSupplies().herbs < config.num_herbs()){
-                        renderMinimapObject(graphics, config.supplySpotsColor(), spot);
-                    }
-                    break;
-                default:
-                    break;
+        if (plugin.getSupplies().fish < config.num_fish()){
+            for (GameObject spot: plugin.getFishing_spots().values()){
+                renderMinimapSupplySpot(graphics,config.supplySpotsColor(),spot);
+            }
+        }
+        if (plugin.getSupplies().ore < config.num_resources()){
+            for (GameObject spot: plugin.getMining_spots().values()){
+                renderMinimapSupplySpot(graphics, config.supplySpotsColor(), spot);
+            }
+        }
+        if (plugin.getSupplies().bark < config.num_resources()){
+            for (GameObject spot: plugin.getBark_spots().values()){
+                renderMinimapSupplySpot(graphics, config.supplySpotsColor(), spot);
+            }
+        }
+        if (plugin.getSupplies().linum < config.num_resources()){
+            for (GameObject spot: plugin.getLinum_spots().values()){
+                renderMinimapSupplySpot(graphics, config.supplySpotsColor(), spot);
+            }
+        }
+        if (plugin.getSupplies().herbs < config.num_herbs()){
+            for (GameObject spot: plugin.getHerb_spots().values()){
+                renderMinimapSupplySpot(graphics, config.supplySpotsColor(), spot);
             }
         }
     }
 
-    private void renderMinibosses(Graphics2D graphics){
-        for (NPC npc : plugin.getBosses()){
-            if (npc.getName().toLowerCase().contains("beast")){
-                renderMinimapActor(graphics,  new Color(0,100,0), npc);
-            }else if (npc.getName().toLowerCase().contains("dragon")){
-                renderMinimapActor(graphics,  Color.RED, npc);
-            }if (npc.getName().toLowerCase().contains("bear")){
-                renderMinimapActor(graphics,  Color.BLUE, npc);
-            }
-        }
-    }
-
-    private void renderMonsters(Graphics2D graphics){
-        for (NPC npc : plugin.getMonsters()){
-            renderMinimapActor(graphics, config.monstersColor(), npc);
-        }
-    }
-
-    private void renderMinimapObject(Graphics2D graphics, Color color, GameObject object){
+    private void renderMinimapSupplySpot(Graphics2D graphics, Color color, GameObject object){
         Point minimapLocation = object.getMinimapLocation();
         if (minimapLocation != null)
         {
             OverlayUtil.renderMinimapLocation(graphics, minimapLocation, color);
         }
     }
-
-    private void renderMinimapActor(Graphics2D graphics, Color color, Actor actor){
-        Point minimapLocation = actor.getMinimapLocation();
-        if (minimapLocation != null)
-        {
-            OverlayUtil.renderMinimapLocation(graphics, minimapLocation, color);
-        }
-    }
-
-
 
 
 
