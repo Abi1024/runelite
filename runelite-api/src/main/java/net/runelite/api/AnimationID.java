723fc39ba6110d66136f5f802109597bd2d665ca
Abi1024 <abiyaz.chowdhury@stonybrook.edu>
2019-10-06 18:03:10 -0400

    Added inferno

--- runelite-api/src/main/java/net/runelite/api/AnimationID.java
+++ runelite-api/src/main/java/net/runelite/api/AnimationID.java
@@ -199,6 +199,23 @@ public final class AnimationID
 	public static final int FARMING_MIX_ULTRACOMPOST = 7699;
 	public static final int FARMING_HARVEST_ALLOTMENT = 830;
 
+	// INFERNO animations
+	public static final int JAL_NIB = 7574;
+	public static final int JAL_MEJRAH = 7578;
+	public static final int JAL_AK_RANGE_ATTACK = 7581;
+	public static final int JAL_AK_MELEE_ATTACK = 7582;
+	public static final int JAL_AK_MAGIC_ATTACK = 7583;
+	public static final int JAL_IMKOT = 7597;
+	public static final int JAL_XIL_MELEE_ATTACK = 7604;
+	public static final int JAL_XIL_RANGE_ATTACK = 7605;
+	public static final int JAL_ZEK_MAGE_ATTACK = 7610;
+	public static final int JAL_ZEK_MELEE_ATTACK = 7612;
+	public static final int JALTOK_JAD_MELEE_ATTACK = 7590;
+	public static final int JALTOK_JAD_MAGE_ATTACK = 7592;
+	public static final int JALTOK_JAD_RANGE_ATTACK = 7593;
+	public static final int TZKAL_ZUK = 7566;
+	public static final int JAL_MEJJAK = 2858;
+
 	// Lunar spellbook
 	public static final int ENERGY_TRANSFER_VENGEANCE_OTHER = 4411;
 	public static final int MAGIC_LUNAR_SHARED = 4413; // Utilized by Fertile Soil, Boost/Stat Potion Share, NPC Contact, Bake Pie
